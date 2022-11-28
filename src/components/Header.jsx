88b7a17a4a9b924c396e2f0c870dd822fa815036
nabilk11 <khan.nabiln11.com>
2022-11-28 17:48:20 -0500

    created basic header component, and imported to main app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h2 className="header">Expense Tracker</h2>
+    </div>
+  );
+};
+
+export default Header;
