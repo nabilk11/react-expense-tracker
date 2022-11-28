ee0a3f96320b48ba040a0851c5f7306b7dccd8d1
nabilk11 <khan.nabiln11.com>
2022-11-28 18:40:09 -0500

    added local state to add transaction form via useState hook

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTransaction = () => {
+    const [text, setText] = useState('')
+    const [amount, setAmount] = useState(0)
+
+
+    
   return (
     <div>
       <h3>Add New Transaction</h3>
       <form >
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter Text..." />
+          <input type="text" placeholder="Enter Text..." value={text} onChange={ (e) => setText(e.target.value) } />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" placeholder="Enter Amount..." />
+          <input type="number" placeholder="Enter Amount..." value={amount} onChange={ (e) => setAmount(e.target.value) } />
         </div>
         <button className="btn">Add transaction</button>
       </form>
