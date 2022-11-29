cf9569e5f3e06459e9b37b29dbab0aacb5da53a0
nabilk11 <khan.nabiln11.com>
2022-11-28 20:40:16 -0500

    brought in transactions to Transactions component and mapped through

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Transactions = () => {
+    // destructured transactions from the Global Context
+    const { transactions } = useContext(GlobalContext)
+
+
   return (
     <div>
       <h3>Transaction History</h3>
       <ul id="list" className="list">
+      { transactions.map(transaction => (
         <li className="minus">
-          Cash <span>-$400</span>
+          { transaction.text } <span >${transaction.amount.toFixed(2)}</span>
           <button className="delete-btn">x</button>
         </li>
+      )) }
+        
       </ul>
     </div>
   );
